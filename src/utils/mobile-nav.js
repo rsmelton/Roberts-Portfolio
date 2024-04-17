f64bc084dad2c21af7dff5ab3af55184e8a87406
Robert Melton <rsmelton@crimson.ua.edu>
2024-04-17 02:03:54 -0500

    fixed bug where at certain breakpoint the mobile nav menu wouldn't go away

--- src/utils/mobile-nav.js
+++ src/utils/mobile-nav.js
@@ -1,7 +1,9 @@
 const mobileNav = () => {
     const headerBtn = document.querySelector('.header__bars');
+    // const headerLinks = document.querySelectorAll('header__link');
     const mobileNav = document.querySelector('.mobile-nav');
     const mobileLinks = document.querySelectorAll('.mobile-nav__link');
+    const media = window.matchMedia("(min-width: 768px)");
 
     // State-like variable
     let isMobileNavOpen = false;
@@ -18,6 +20,11 @@ const mobileNav = () => {
         }
     };
 
+    // const handleHeaderLinkClick = () => {
+    //     isMobileNavOpen = false;
+    //     document.body.style.overflow = 'auto';
+    // };
+
     const handleMobileLinkClick = () => {
         isMobileNavOpen = false;
         mobileNav.style.display = 'none';
@@ -27,9 +34,21 @@ const mobileNav = () => {
     // Events
     headerBtn.addEventListener('click', handleHeaderBtnClick);
 
+    // headerLinks.forEach(link => 
+    //     link.addEventListener('click', handleHeaderLinkClick)
+    // );
+
     mobileLinks.forEach(link => 
         link.addEventListener('click', handleMobileLinkClick)
     );
+
+    media.addEventListener('change', () => {
+        if (media.matches) {
+            isMobileNavOpen = false;
+            mobileNav.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }
+    });
 };
 
 export default mobileNav;
